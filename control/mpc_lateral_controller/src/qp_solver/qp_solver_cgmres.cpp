589326e17e4d4a5660ae72447f0a9612596ebeac
kyoichi-sugahara <kyoichi.sugahara@tier4.jp>
2024-05-22 03:05:07 +0900

    feat: add logging of GMRES iterations in QPSolverCGMRES
    Signed-off-by: kyoichi-sugahara <kyoichi.sugahara@tier4.jp>

--- control/mpc_lateral_controller/src/qp_solver/qp_solver_cgmres.cpp
+++ control/mpc_lateral_controller/src/qp_solver/qp_solver_cgmres.cpp
@@ -101,7 +101,8 @@ bool QPSolverCGMRES::solveCGMRES(
     // RCLCPP_DEBUG(logger_, " time_from_last_initialized = %e \n", time_from_last_initialized);
     // RCLCPP_DEBUG(logger_, "updated u = %e ",mpc_.uopt()[0].value());
     cgmres_logger_.save(
-      time_from_last_initialized, x, mpc_.uopt()[0], mpc_.uopt(), mpc_.optError());
+      time_from_last_initialized, x, mpc_.uopt()[0], mpc_.uopt(), mpc_.optError(),
+      mpc_.gmres_iter());
     // cgmres_logger_.save(
     //   time_from_last_initialized, x, mpc_.uopt()[0], mpc_.uopt(), mpc_.optError(),
     //   mpc_.normDiff(), mpc_.StandardDeviation());
