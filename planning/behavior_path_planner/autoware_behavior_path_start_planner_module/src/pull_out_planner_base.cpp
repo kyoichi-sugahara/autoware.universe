48308ac6896910d5f6c08cc91990236f9fac556b
kyoichi-sugahara <kyoichi.sugahara@tier4.jp>
2024-12-11 18:45:21 +0900

    fix(pull_out_planner): add null check for dynamic_objects in collision detection
    Signed-off-by: kyoichi-sugahara <kyoichi.sugahara@tier4.jp>

--- planning/behavior_path_planner/autoware_behavior_path_start_planner_module/src/pull_out_planner_base.cpp
+++ planning/behavior_path_planner/autoware_behavior_path_start_planner_module/src/pull_out_planner_base.cpp
@@ -25,7 +25,7 @@ bool PullOutPlannerBase::isPullOutPathCollided(
   // check for collisions
   const auto & dynamic_objects = planner_data_->dynamic_object;
   if (!dynamic_objects) {
-    return false;  // もしくは適切なエラーハンドリング
+    return false;
   }
   const auto pull_out_lanes = start_planner_utils::getPullOutLanes(
     planner_data_, planner_data_->parameters.backward_path_length + parameters_.max_back_distance);
