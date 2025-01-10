ab3275eb2ea07f047699cfb4e3e5345bbc74073b
Kyoichi Sugahara <kyoichi.sugahara@tier4.jp>
2025-01-10 20:03:13 +0900

    fix(freespace_pull_out): remove unused vehicle_info retrieval
    Signed-off-by: Kyoichi Sugahara <kyoichi.sugahara@tier4.jp>

--- planning/behavior_path_planner/autoware_behavior_path_start_planner_module/src/freespace_pull_out.cpp
+++ planning/behavior_path_planner/autoware_behavior_path_start_planner_module/src/freespace_pull_out.cpp
@@ -32,7 +32,6 @@ namespace autoware::behavior_path_planner
 FreespacePullOut::FreespacePullOut(rclcpp::Node & node, const StartPlannerParameters & parameters)
 : PullOutPlannerBase{node, parameters}, velocity_{parameters.freespace_planner_velocity}
 {
-  const auto vehicle_info = autoware::vehicle_info_utils::VehicleInfoUtils(node).getVehicleInfo();
   autoware::freespace_planning_algorithms::VehicleShape vehicle_shape(
     vehicle_info_, parameters.vehicle_shape_margin);
   if (parameters.freespace_planner_algorithm == "astar") {
