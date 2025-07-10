988a0125b9bbdf8fd32051dbfd6756546820ff60
Kyoichi Sugahara <kyoichi.sugahara@tier4.jp>
2025-07-11 02:25:31 +0900

    temp
    Signed-off-by: Kyoichi Sugahara <kyoichi.sugahara@tier4.jp>

--- planning/behavior_path_planner/autoware_behavior_path_planner/src/behavior_path_planner_node.cpp
+++ planning/behavior_path_planner/autoware_behavior_path_planner/src/behavior_path_planner_node.cpp
@@ -718,11 +718,32 @@ PathWithLaneId::SharedPtr BehaviorPathPlannerNode::getPath(
                                           : planner_data->prev_output_path;
   path->header = planner_data->route_handler->getRouteHeader();
 
+  // --- デバッグ出力追加: pathの各点のidx, x, y, yaw[rad] ---
+  // if (path && !path->points.empty()) {
+  //   for (size_t i = 0; i < path->points.size(); ++i) {
+  //     const auto & p = path->points[i].point.pose.position;
+  //     double yaw = tf2::getYaw(path->points[i].point.pose.orientation);
+  //     std::cerr << "[bpp_path] idx=" << i << " x=" << p.x << " y=" << p.y << " yaw=" << yaw << "
+  //     rad" << std::endl;
+  //   }
+  // }
+  // --- デバッグ出力ここまで ---
+
   PathWithLaneId connected_path;
   const auto module_status_ptr_vec = planner_manager->getSceneModuleStatus();
 
   const auto resampled_path = utils::resamplePathWithSpline(
     *path, planner_data->parameters.output_path_interval, keepInputPoints(module_status_ptr_vec));
+
+  // --- デバッグ出力追加: resampled_pathの各点のidx, x, y, yaw[rad] ---
+  // for (size_t i = 0; i < resampled_path.points.size(); ++i) {
+  //   const auto & p = resampled_path.points[i].point.pose.position;
+  //   double yaw = tf2::getYaw(resampled_path.points[i].point.pose.orientation);
+  //   std::cerr << "[bpp_resampled] idx=" << i << " x=" << p.x << " y=" << p.y << " yaw=" << yaw <<
+  //   " rad" << std::endl;
+  // }
+  // --- デバッグ出力ここまで ---
+
   return std::make_shared<PathWithLaneId>(resampled_path);
 }
 
